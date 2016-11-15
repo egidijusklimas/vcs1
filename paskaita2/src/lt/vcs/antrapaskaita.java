a08f491d9dec2be76c50ed8e168a23d064896db4
Egidijus Klimas <egidijus.klimas@yahoo.com>
2016-11-15 14:20:40 -0500

    Antros Paskaitos kodas

---
+++ paskaita2/src/lt/vcs/antrapaskaita.java
@@ -0,0 +1,36 @@
+
+package lt.vcs;
+
+/**
+ *
+ * @author Q
+ */
+public class antrapaskaita {
+    
+    static Float kas;
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Antra paskaita");
+        
+        byte baitas = 77;           Byte baitas2 = new Byte(baitas);
+        baitas = 88;
+        short trumpas = 15325;      Short trumpas2 = new Short(trumpas);
+        int intas = 777777777;      Integer intas2 = new Integer(555);
+        long longas = 8888888888888888888L;     Long longas2 = new Long(888);
+        float floatas = 255.55555555F;  Float floatas2 = new Float(25.5);
+        double doulbas = 255.66666666666666;  Double double2 = new Double(511.5);
+        char charas = 'a';      Character charas2 = new Character('a');
+        boolean bulijonas = false;      Boolean bulijonas2 = new Boolean(true);
+        
+       int _labas = 0;
+       
+       
+       System.out.println(kas);
+        
+    }
+    
+}
